cd0ec465a01a7dd27e5f2e8561536a44190f2bfd
Damyan Maxwell <saintnyte@gmail.com>
2025-02-14 10:26:24 +0100

    04 af

--- M3Prog_project/public/01/phphtml_var.php
+++ M3Prog_project/public/01/phphtml_var.php
@@ -4,24 +4,20 @@ $title = "Mijn mooie website";
 $datum = date('d-m-Y'); 
 $tijd = date("H:i"); 
 $favorieteGame = "Super metroid"; 
-$mooisteGetal = 42;
-
-echo $title;
-echo $datum;
-echo $tijd;
-echo $favorieteGame;
-echo $mooisteGetal;
-?> 
-<!doctype html>
+$mooisteGetal = 77;
+$favorieteShow = "Amazing World of Gumball";
+$leuksteStad = "Amsterdam";
 ?>
 <!doctype html>
 <html lang="en">
     <head>
-        <title><?php echo "Titel van de pagina" ?></title>
+        <title><?php echo $title?></title>
     </head>
     <body>
-        <h2>De datum vandaag is: <?php echo date('d-m-Y')?>, en de tijd is <?php echo date("H:i")?> uur.
-        
-        <p><h2>Mijn favoriete game</h2><?php echo "Super metroid";?></p>
+        <h2>De datum vandaag is: <?php echo $datum?>, en de tijd is <?php echo $tijd?> uur.
+        <p><h2>Het mooiste Getal:</h2><?= $mooisteGetal ?></p>
+        <p><h2>Mijn favoriete game</h2><?php echo $favorieteGame?></p>
+        <p><h2>Mijn favoriete show</h2><?php echo $favorieteShow?></p>
+        <p><h2>Het leukste stad</h2><?php echo $leuksteStad?></p>
     </body>
 </html>
