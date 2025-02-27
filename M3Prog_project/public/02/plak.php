1a2052a35e2ed5c50d703c8612316d564f91d986
Damyan Maxwell <saintnyte@gmail.com>
2025-02-27 10:09:11 +0100

    01 af

---
+++ M3Prog_project/public/02/plak.php
@@ -0,0 +1,12 @@
+<?php
+
+$standaardString = 'hello world<br>';
+$extraString = 'extra world<br>';
+$voornaam = 'Luigi';
+$achternaam = 'mario';
+$fullname = "$voornaam" ." ". "$achternaam" . "<br>";
+
+echo $standaardString;
+echo $extraString;
+echo $fullname;
+?>
