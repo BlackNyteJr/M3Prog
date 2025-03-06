aec60ad32b5fdea47a08dea7698af975f7da798b
Damyan Maxwell <saintnyte@gmail.com>
2025-03-06 14:26:00 +0100

    05 af

--- M3Prog_project/public/03/korting.php
+++ M3Prog_project/public/03/korting.php
@@ -16,25 +16,22 @@ if($totaalBesteld >= 250)
     $korting = 0.05;
 }
 
-if($totaalBesteld >= 400)
-{
-    $cadeautje = true;
-}
-
-if($totaalBesteld >= 1000)
-{
-    $korting = 0.075;
-    $cadeauWaarde = true;
-}
 
 $totaal = $totaalBesteld - ($korting*$totaalBesteld) + ($verzendKosten);
 
 echo "Totaal van de bestelde artiekelen $totaalBesteld<br>";
 echo "Totaal met verzendkosten wordt het $totaal<br>";
 
-if($cadeautje = true)
+if($totaalBesteld >= 400 == $totaalBesteld < 1000.0)
 {
+    $cadeautje = true;
     echo "U krijgt ook een cadeautje ter waarde van 10 euro<br>";
 }
 
+if($totaalBesteld >= 1000)
+{
+    $korting = 0.075;
+    $cadeauWaarde = true;
+    echo "U krijgt ook een cadeautje ter waarde van 40 euro<br>";
+}
 ?>
