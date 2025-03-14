aff464ea6034ff7dcc58bfffa8ecb8c7f9f60163
Damyan Maxwell <saintnyte@gmail.com>
2025-03-14 10:38:02 +0100

    les 05 map 1 01 af

---
+++ M3Prog_project/public/05/phpfunctions.php
@@ -0,0 +1,10 @@
+<?php
+
+$hoeLangIsDitWoord = "scrabbleWoord";
+$woordLengte = strlen($hoeLangIsDitWoord);
+$hoeLangIsDitWoord2 = "Supercalifragilisticexpialidocious";
+$woordLengte2 = strlen($hoeLangIsDitWoord2);
+
+print($woordLengte);
+echo "<br>";
+print($woordLengte2);
