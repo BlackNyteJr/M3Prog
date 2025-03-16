2c1fdba66b48521ccd50d1fed03d22744a79edb6
Damyan Maxwell <saintnyte@gmail.com>
2025-03-16 22:38:57 +0100

    Mario deelte toegevoegd van pc

--- M3Prog_project/public/05/phpfunctions.php
+++ M3Prog_project/public/05/phpfunctions.php
@@ -1,16 +1,42 @@
 <?php
-
 $hoeLangIsDitWoord = "scrabbleWoord";
 $woordLengte = strlen($hoeLangIsDitWoord);
-$hoeLangIsDitWoord2 = "Supercalifragilisticexpialidocious";
-$woordLengte2 = strlen($hoeLangIsDitWoord2);
-$tweeMacht2 = pow(2, 2);
-$tweeMacht10 = pow(2, 10);
 
 print($woordLengte);
-echo "<br>";
-print($woordLengte2);
-echo "<br>";
-echo $tweeMacht2;
-echo "<br>";
-echo $tweeMacht10;
+print "</br>";
+
+//vraag 1, het is een function
+//vraag 2, het is een unsigned integer type
+//vraag 3, de hoeveelheid tekens in het woord inclusief spaties
+
+$woord = "Supercalifragilisticexpialidocious";
+$lengteWoord = strlen($woord);
+
+print($lengteWoord);
+
+//vraag 4, 2x
+
+print "</br>";
+
+$tweeMacht2 = pow(2,10);
+
+print($tweeMacht2);
+print "</br>";
+print(pow(2,10));
+print "</br>";
+
+//vraag 1, door print
+//https://www.php.net/manual/en/function.strtoupper.php
+
+$naam = "wario";
+print($naam);
+
+print "</br>";
+
+$naamGroot = strtoupper($naam);
+print($naamGroot);
+
+print "</br>";
+
+$naamKlein = strtolower($naamGroot);
+print($naamKlein);
