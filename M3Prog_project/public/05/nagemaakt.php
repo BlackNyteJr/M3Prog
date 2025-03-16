1ca87f22a52e18f40d6240407fd3673059306afc
Damyan Maxwell <saintnyte@gmail.com>
2025-03-16 22:00:18 +0100

    05/01 af & 05/02/01 af

---
+++ M3Prog_project/public/05/nagemaakt.php
@@ -0,0 +1,14 @@
+<?php
+
+$dezeGaatMee = "deze waarde wordt meegenomen"
+
+$printText = "Regel 1<br>
+Regel 2<br>
+Regel 3<br>";
+
+function mijnPrint($printText)
+{
+    print($printText);
+}
+
+mijnPrint();
--- M3Prog_project/public/05/phpfunctions.php
+++ M3Prog_project/public/05/phpfunctions.php
@@ -4,7 +4,13 @@ $hoeLangIsDitWoord = "scrabbleWoord";
 $woordLengte = strlen($hoeLangIsDitWoord);
 $hoeLangIsDitWoord2 = "Supercalifragilisticexpialidocious";
 $woordLengte2 = strlen($hoeLangIsDitWoord2);
+$tweeMacht2 = pow(2, 2);
+$tweeMacht10 = pow(2, 10);
 
 print($woordLengte);
 echo "<br>";
 print($woordLengte2);
+echo "<br>";
+echo $tweeMacht2;
+echo "<br>";
+echo $tweeMacht10;
