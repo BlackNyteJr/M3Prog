20b764d17545044a29228842e2d51c49f4365b0b
Damyan Maxwell <saintnyte@gmail.com>
2025-03-20 09:59:18 +0100

    04 af

--- M3Prog_project/public/04/arrays_eigen.php
+++ M3Prog_project/public/04/arrays_eigen.php
@@ -2,8 +2,11 @@
 
 $games = ["Call of Duty", "Assassin's Creed", "Minecraft", "Battlefield", "Grand Theft Auto"];
 
+array_push($games, "Outerwilds");
+
 print_r($games);
 echo "<br>";
-echo $games[2];
+echo count($games);
 echo "<br>";
-echo $games[4];
+sort($games);
+echo implode(' ,<br> ', $games);
