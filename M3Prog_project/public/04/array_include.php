9e41a625de82626a2ddeb74f77fabd9d6e7cae66
Damyan Maxwell <saintnyte@gmail.com>
2025-03-20 17:24:34 +0100

    half klaar

---
+++ M3Prog_project/public/04/array_include.php
@@ -0,0 +1,27 @@
+<?php
+
+$announcements = [
+    "a.html",
+    "b.html",
+    "c.html"
+];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i=0; $i < ($announcements); $i++) { 
+        $announcements = $announcements[$i];
+        include "announcements/$announcements";
+    };
+    ?>
+</body>
+</html>
