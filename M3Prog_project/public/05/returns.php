c72a456286110c06a907d1f48008cc4ae5081e9a
Damyan Maxwell <saintnyte@gmail.com>
2025-03-27 09:52:37 +0100

    functions/03/06 af

--- M3Prog_project/public/05/returns.php
+++ M3Prog_project/public/05/returns.php
@@ -1,11 +1,23 @@
 <?php
 
 $mijnFunctionResultaat = mijnFunction();
+$returnVariableResultaat = returnVariable();
 
 function mijnFunction()
 {
-    //hier komt de code van de function
     return 1;
 }
 
+function returnVariable()
+{
+    $result = rand();
+    return $result;
+}
+
 print_r($mijnFunctionResultaat);
+echo "<br>";
+print_r($returnVariableResultaat);
+echo "<br>";
+print_r($returnVariableResultaat);
+echo "<br>";
+print_r($returnVariableResultaat);
